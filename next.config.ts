e8e0819b5cbed99cd9423fdd2697f9c616b4c3c4
hungdoba <hungdoba.hdb@gmail.com>
2025-06-07 13:20:09 +0900

    Comment out output configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export',
   images: {
     loader: 'custom',
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
